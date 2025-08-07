15ff8b65ef5426a311138376caa9e23ae41b4adc
subcakewei <weiyashuai.30042538@huawei.com>
2025-08-07 09:17:13 +0000

    修正子系统ID与模块ID
    Signed-off-by: subcakewei <weiyashuai.30042538@huawei.com>

--- services/common/sharing_hisysevent.cpp
+++ services/common/sharing_hisysevent.cpp
@@ -24,8 +24,8 @@ static constexpr char ORG_PKG[] = "sharing_service";
 static constexpr char HOST_PKG[] = "cast_engine_service";
 static constexpr char EVENT_NAME[] = "MIRACAST_BEHAVIOR";
 
-static const uint8_t WIFI_DISPLAY_SYSTEM_ID = 227;
-static const uint8_t WIFI_DISPLAY_MODULE_ID = 11;
+static const uint8_t WIFI_DISPLAY_SYSTEM_ID = 208;
+static const uint8_t WIFI_DISPLAY_MODULE_ID = 19;
 
 SharingHiSysEvent::SharingHiSysEvent(BIZSceneType scene, std::string callPkg)
     : bizScene_(static_cast<int32_t>(scene)), toCallPkg_(callPkg)
